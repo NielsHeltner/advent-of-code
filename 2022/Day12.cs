37912a7728db561b265d9f578fd1a9248de4ff13
Niels Heltner <niels.heltner@hotmail.com>
2022-12-19 21:33:27 +0100

    day 12 part 2

--- 2022/Day12.cs
+++ 2022/Day12.cs
@@ -5,10 +5,68 @@ public class Day12
     public async Task RunAsync()
     {
         var input = await File.ReadAllLinesAsync("Day12Input.txt");
-        var nodes = input.Select(x => x.Select(y => new Node(y)).ToList()).ToList();
+
+        Part1(input);
+
+        Part2(input);
+    }
+
+    void Part2(string[] input)
+    {
+        var nodes = ParseNodes(input);
+
+        var minSteps = int.MaxValue;
+
+        var starts = nodes.SelectMany(x => x.Select(y => y)).Where(x => x.Value == 'a' || x.Value == 'S').ToList();
+        var end = nodes.SelectMany(x => x.Select(y => y)).First(x => x.Value == 'E');
+        foreach (var start in starts)
+        {
+            DijkstraSearch(start, end);
+            var steps = GetStepCount(end);
+            if (steps <= 0)
+                continue;
+
+            minSteps = Math.Min(minSteps, steps);
+            foreach (var nodeList in nodes)
+                foreach (var node in nodeList)
+                {
+                    node.PathToStart = null;
+                    node.MinCostToStart = null;
+                    node.Visited = false;
+                }
+        }
+
+        Console.WriteLine(minSteps);
+    }
+
+    void Part1(string[] input)
+    {
+        var nodes = ParseNodes(input);
 
         var start = nodes.SelectMany(x => x.Select(y => y)).First(x => x.Value == 'S');
         var end = nodes.SelectMany(x => x.Select(y => y)).First(x => x.Value == 'E');
+
+        DijkstraSearch(start, end);
+
+        Console.WriteLine(GetStepCount(end));
+    }
+
+    int GetStepCount(Node end)
+    {
+        var shortestPath = new List<Node>();
+        var current = end;
+        while (current != null)
+        {
+            shortestPath.Add(current);
+            current = current.PathToStart;
+        }
+
+        return shortestPath.Count - 1;
+    }
+
+    List<List<Node>> ParseNodes(string[] input)
+    {
+        var nodes = input.Select(x => x.Select(y => new Node(y)).ToList()).ToList();
 
         for (var rowI = 0; rowI < nodes.Count; rowI++)
         {
@@ -36,6 +94,11 @@ public class Day12
             }
         }
 
+        return nodes;
+    }
+
+    void DijkstraSearch(Node start, Node end)
+    {
         start.MinCostToStart = 0;
         
         var prioQueue = new List<Node>();
@@ -54,7 +117,7 @@ public class Day12
                 var costToChildNode = node.MinCostToStart + edge.Cost;
                 
                 if (childNode.MinCostToStart == null ||
-                    childNode.MinCostToStart > costToChildNode)
+                    childNode.MinCostToStart >= costToChildNode)
                 {
                     childNode.MinCostToStart = costToChildNode;
                     childNode.PathToStart = node;
@@ -69,16 +132,6 @@ public class Day12
             if (node == end)
                 break;
         }
-
-        var shortestPath = new List<Node>();
-        var current = end;
-        while (current != null)
-        {
-            shortestPath.Add(current);
-            current = current.PathToStart;
-        }
-
-        Console.WriteLine(shortestPath.Count - 1);
     }
 
     Edge? GetEdge(int height, List<List<Node>> nodes, int rowI, int colI)
